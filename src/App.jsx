deb0569f70c24fea79778cd1d2a48a24dfeecfd8
Ayush Singh <ayushsingh842018@gmail.com>
2026-02-03 18:52:59 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/Mainlayout";
+import MainLayout from "./layouts/Mainlayout.jsx";
 import Home from "./pages/Home";
 import ProspectArgument from "./pages/ProspectArgument";
 
